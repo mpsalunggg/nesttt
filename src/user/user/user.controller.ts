cb2e4876b03c83fab9768aa5f0d7925ce0556662
mpsalunggg <putrasatria893@gmail.com>
2025-03-19 22:15:23 +0700

    test method post, and params request id

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Req } from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('/api/user')
 export class UserController {
   @Get('/sample')
   get(): string {
     return 'Hello World';
+  }
+
+  @Post('/test-post')
+  post(): string {
+    return 'Testt Post';
+  }
+
+  @Get('/:id')
+  getById(@Req() request: Request): string {
+    return `Get by id : ${request.params.id}`;
   }
 }
