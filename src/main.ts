02b3b38b6b5c9ad5ad90438cd8799dbfc4349d7f
mpsalunggg <putrasatria893@gmail.com>
2025-03-21 01:16:23 +0700

    setup mustache express

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as mustache from 'mustache-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(cookieParser('COOKIE TEST'));
+
+  app.set('views', __dirname, '/../views');
+  app.set('view engine', 'html');
+
+  app.engine('html', mustache);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
