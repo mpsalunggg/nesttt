afa3ba2d3bbbaad42571eafc142374e82fd43572
mpsalunggg <putrasatria893@gmail.com>
2025-03-23 00:25:49 +0800

    testing with node-mocks-htpp

--- src/user/user/user.controller.spec.ts
+++ src/user/user/user.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import * as httpMock from 'node-mocks-http';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -18,5 +19,17 @@ describe('UserController', () => {
 
   it('should return "Hello World!"', () => {
     expect(controller.post()).toBe('Testt Post');
+  });
+
+  it('should can view  template', async () => {
+    const response = httpMock.createResponse();
+    controller.getViewHello('mps', 'frontend', response);
+
+    expect(response._getRenderView()).toBe('index.html');
+    expect(response._getRenderData()).toEqual({
+      name: 'mps',
+      role: 'frontend',
+      title: 'Template',
+    });
   });
 });
